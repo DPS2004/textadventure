960cf7fb2f4bdd83e7962d6cd196ffca3763003d
DPS2004 <iheartscratch@gmail.com>
2019-03-26 09:33:15 -0400

    made all dialog unsucky

--- detective.py
+++ detective.py
@@ -33,9 +33,9 @@ def findline(linenum):
     return line
 
 
-def saveall(sname, senemyname, scheckpoint,):
+def saveall(sname, shatname, scheckpoint,):
     file = open("savefile.sav", "w")
-    file.writelines((line + '\n' for line in [sname, senemyname, n2s(scheckpoint)]))
+    file.writelines((line + '\n' for line in [sname, shatname, n2s(scheckpoint)]))
     file.close()
 
 
@@ -53,17 +53,17 @@ def clear():
         _ = system('clear')
 
 
-def dialog(spritename, text, loops, animdelay):
+def dialog(spritename, displayname, text, loops, animdelay):
     for x in range(loops):
         clear()
         printtxt("ascii/"+spritename+"1.txt")
         print(" ")
-        print(" |", cname + ":", text, "|")
+        print(" |", displayname + ":", text, "|")
         time.sleep(animdelay)
         clear()
         printtxt("ascii/"+spritename+"2.txt")
         print(" ")
-        print(" |", cname + ":", text, "|")
+        print(" |", displayname + ":", text, "|")
         time.sleep(animdelay)
     print("(press key)")
     waitforkey()
@@ -75,7 +75,7 @@ def question(prompt):
 
 
 def yesno(inputyn):
-    if inputyn.lower() in ["yes", "y", "yay", "yep", "sure", "yeah", "yeet", "mhm", "positively"]:
+    if inputyn.lower() in ["yes", "y", "yay", "yep", "sure", "yeah", "yeet", "mhm", "positively", "indeedily-doo"]:
         return "y"
     else:
         return "n"
@@ -84,7 +84,7 @@ def yesno(inputyn):
 if yesno(question("Welcome to detective game! would you like to load a saved game?")) == "y":
     if os.path.isfile("./savefile.sav"):
         cname = findline(1)
-        enemyname = findline(2)
+        hatname = findline(2)
         checkpoint = s2n(findline(3))
 
     else:
@@ -95,7 +95,7 @@ else:
     cname = question("what do you want to name your detective? ")
     if cname == "debug":
         cname = "Detective"
-        enemyname = "Masked man"
+        hatname = "Top hat man"
         print("debug mode activated!")
         checkpoint = s2n(question("warp to what checkpoint?"))
     else:
@@ -103,31 +103,38 @@ else:
 if checkpoint == 0:
     print("you are detective", cname + ", ready to solve crimes and stuff")
     waitforkey()
-    dialog("detective", "oh boy i sure love being a detective", 4, 0.2)
-    enemyname = question("a masked stranger come up to you. What is his name?")
-    dialog("masked", "yo whats up mr detective man i need some detective crap done for me my dude", 5, 0.25)
-    dialog("detective", "oh man oh jeez ok sure what will i get in return", 6, 0.15)
-    dialog("masked", "you will get a magical artifact i totally didn't imbue with a curse", 5, 0.25)
-    dialog("detective", "seems legit", 2, 0.2)
+    dialog("detective", cname, "Time to open up the office.", 4, 0.2)
+    hatname = question("The door opens, and a man with a strange hat comes up to you. What is his name?")
+    dialog("tophatmutter", hatname, "*mutters something about a detective", 5, 0.25)
+    dialog("detective", cname, "Can you speak up please?", 5, 0.15)
+    clear()
+    print("The man begins to yell.")
+    waitforkey()
+    dialog("tophatyell", hatname, "YES HELLO THERE IS THIS THE DETECTIVE PLACE I NEED A DETECTIVE FOR MY DETEECTIVATING NEEDS", 7, 0.25)
+    dialog("detectiveangry", cname, "Indoor voice please!", 2, 0.2)
+    dialog("tophatyell", hatname, "THIS IS INDOOR VOICE", 4, 0.2)
+    dialog("detectiveangry",cname, "I am sure that you have woken up the entire neighborhood by now.", 6, 0.18)
+    dialog("detective", cname, "Can you talk at my volume?", 4, 0.2)
+    dialog("tophatyell", hatname, "Indeedily-doo.", 3, 0.25)
     checkpoint = "1"
-    saveall(cname,enemyname,checkpoint)
+    saveall(cname,hatname,checkpoint)
 if checkpoint == 1:
-    dialog("masked", "someone broke in to my house. who the heck did it?", 5, 0.25)
-    dialog("detective", "who are the suspects?", 3, 0.15)
-    dialog("masked", "Casey, who is my dog, Mrs. Greene, who is my cranky old neighbor, and Carson, who is my ex best friend", 12, 0.25)
-    dialog("detective", "wait hold up why is your dog in the lineup?", 2, 0.2)
-    dialog("masked", "he can be a bit of a jerk, i wouldn't put it past him", 3, 0.25)
+    dialog("tophatyell", hatname, "Someone has broked in to house. Who the heck did?", 5, 0.25)
+    dialog("detective", cname, "who are the suspects?", 3, 0.15)
+    dialog("tophatyell", hatname, "Casey, who is my dog, Mrs. Greene, who is meanie old neighbor, and Carson, who is used to be best friend", 12, 0.25)
+    dialog("detectiveangry", cname, "wait hold up why is your dog in the lineup?", 2, 0.2)
+    dialog("tophatyell", hatname, "He can be jerk, would not put past him", 3, 0.25)
     if yesno(question("take up the case? ")) == "y":
         dialog("detective", "ok, take me to the scene of the crime.", 4, 0.2)
         checkpoint = 2
     else:
-        dialog("detective", "dude, you are crazy, im not doing this", 4, 0.25)
-        dialog("masked", "but don't you want to get that magical artifact??", 4, 0.35)
-        dialog("detective", "...yes", 1, 1)
-        dialog("masked", "then it it is settled!", 4, 0.2)
-        dialog("masked", "to the crime scene!!", 4, 0.2)
+        dialog("detectiveangry", cname, "You are clearly insane. I am not doing this.", 7, 0.25)
+        dialog("tophatyell", hatname, "You have other case to do??", 4, 0.35)
+        dialog("detective", cname, "...no", 1, 1)
+        dialog("tophatyell", "then it it is settled!", 4, 0.2)
+        dialog("tophatyell", "to the crime scene!!", 4, 0.2)
         checkpoint = 2
-    saveall(cname, enemyname, checkpoint)
+    saveall(cname, hatname, checkpoint)
 if checkpoint == 2:
     printtxt("ascii/howtoinvestigate.txt")
     waitforkey()
